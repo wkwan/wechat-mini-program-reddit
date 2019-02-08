266375a4fdb197acb05244898a780c7e3a8a34d0
William Kwan <william3252@gmail.com>
2019-02-07 21:55:06 -0500

    youtube tutorial source code

---
+++ cloudfunctions/upvote/index.js
@@ -0,0 +1,15 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+const db = cloud.database();
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  return await db.collection("posts").doc(event.id).update({
+    data: {
+      votes: db.command.inc(1)
+    }
+  })
+}
